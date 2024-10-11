4f64aa46672089b77c9f325a027c59e8063254de
mateus <mat.marq.oliveira@gmail.com>
2024-10-11 17:10:02 -0300

    Refatorando processos

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AnonymousDto } from '../interfaces/AnonymousDto';
 import { CommandService } from '../services/command.service';
 import { CommonModule } from '@angular/common';
@@ -14,11 +14,53 @@ import { CommandDto } from '../interfaces/CommandDto';
   styleUrls: ['./console.component.css'],
   imports: [CommonModule] 
 })
-export class ConsoleComponent {
+export class ConsoleComponent implements OnInit {
+  private welcomeMessage1: string = 'WELCOME TO OLD SCHOOL SOCIAL MEDIA';
+  private welcomeMessage2: string = 'TYPE HELP TO LEARN HOW TO USE IT';
+  private currentIndex: number = 0;
+  private typingSpeed: number = 100; // Velocidade de digitação (ms por caractere)
+  private erasingSpeed: number = 50; // Velocidade de apagar (ms por caractere)
+  private delayBetweenMessages: number = 1000; // Pausa antes de apagar ou mudar de mensagem
+
+
   command = '';
   history: ConsoleHistoryDto[] = []; // Lista de comandos e respostas
 
   constructor(private commandService: CommandService) {}
+
+  ngOnInit(): void {
+    this.welcomeAnimation();
+  }
+
+  welcomeAnimation() {
+    this.typeMessage(this.welcomeMessage1, () => {
+      setTimeout(() => this.eraseMessage(() => {
+        this.typeMessage(this.welcomeMessage2, () => {
+          setTimeout(() => this.eraseMessage(), this.delayBetweenMessages);
+        });
+      }), this.delayBetweenMessages);
+    });
+  }
+
+  typeMessage(message: string, callback?: () => void) {
+    if (this.currentIndex < message.length) {
+      this.command += message.charAt(this.currentIndex);
+      this.currentIndex++;
+      setTimeout(() => this.typeMessage(message, callback), this.typingSpeed);
+    } else {
+      this.currentIndex = 0;
+      if (callback) callback();
+    }
+  }
+
+  eraseMessage(callback?: () => void) {
+    if (this.command.length > 0) {
+      this.command = this.command.substring(0, this.command.length - 1);
+      setTimeout(() => this.eraseMessage(callback), this.erasingSpeed);
+    } else {
+      if (callback) callback();
+    }
+  }
 
   onKey(event: KeyboardEvent) {
     if (event.key === 'Backspace') {
@@ -29,21 +71,31 @@ export class ConsoleComponent {
       const endpointOption = this.detectEndpoint(this.command);
       if (endpointOption === 0) {
         this.callAnonymous();
-      } else if (endpointOption > 0) {
+      } 
+      
+      if (endpointOption === 1) {
         this.callCommands();
+      }
+
+      if(endpointOption === 2){
+        this.history = [];
+        this.command = '';
       }
     }
   }
 
   detectEndpoint(command: string): number {
     const splitCommand = command.split(" ");
-    
     if (splitCommand[0] === "MAY" && splitCommand[1] === "I" && splitCommand[2] === "COME" && splitCommand[3] === "IN" && splitCommand[4] === "?" && splitCommand[5] === "NICKNAME" && splitCommand[7] === "PASSWORD") {
       return 0;
     }
 
     if (splitCommand[0] === "CREATE" && splitCommand[1] === "USER" && splitCommand[2] === "NICKNAME" && splitCommand[4] === "PASSWORD") {
       return 0;
+    }
+
+    if(splitCommand[0] === "CLEAR"){
+      return 2
     }
 
     return 1;
@@ -53,59 +105,38 @@ export class ConsoleComponent {
     const anonymous: AnonymousDto = {
       content: this.command
     };
-
     this.commandService.sendAnonymousCommand(anonymous).subscribe((res: DefaultResponseDto) => {
       //adiciona mensagem de login
       if(res.messages && res.messages[0] ==="logged"){
-        this.addLoginTreat(res.messages);
+        localStorage.setItem('key',res.messages[2]);
+        this.updateHistory([res.messages[1]],[])
         return;
       }
-      const responseMessages = res.messages?.join('<br/>') || ''; // Concatena mensagens
-      const responseErrors = res.errors?.join('<br/>') || ''; // Concatena erros
-      const response = responseMessages + (responseErrors ? '<br/>' + responseErrors : '');
-      // Adiciona a entrada de histórico
-      this.history.push({ command: this.command, response: response });
-
-      // Limpa o comando atual
-      this.command = '';
-
+      this.updateHistory(res.messages,res.errors);
     }, (err: any) => {
       console.error(err); // Tratamento de erro
     });
   }
 
-  addLoginTreat(res:string[]){
-    localStorage.setItem('key',res[3]);
-    localStorage.setItem('id',res[2]);
-    const responseMessages = res[1]; // Concatena mensagens
-    const response = responseMessages;
+  callCommands() {
+    const command:CommandDto={
+      content:this.command
+    }
+
+    this.commandService.sendCommand(command).subscribe((res:DefaultResponseDto)=>{
+      this.updateHistory(res.messages,res.errors)
+    },(error:any)=>{
+
+    })
+  }
+
+  updateHistory(messages:string[] | null,errors:string[] | null){
+    const responseMessages = messages?.join('<br/>') || ''; // Concatena mensagens
+    const responseErrors = errors?.join('<br/>') || ''; // Concatena erros
+    const response = responseMessages + (responseErrors ? '<br/>' + responseErrors : '');
     // Adiciona a entrada de histórico
     this.history.push({ command: this.command, response: response });
     // Limpa o comando atual
     this.command = '';
-  }
-
-  callCommands() {
-    debugger;
-    const id = localStorage.getItem('id');
-    if(!id) return;
-
-    const command:CommandDto={
-      content:this.command,
-      userId: parseInt(id)
-    }
-
-    this.commandService.sendCommand(command).subscribe((res:DefaultResponseDto)=>{
-      const responseMessages = res.messages?.join('<br/>') || ''; // Concatena mensagens
-      const responseErrors = res.errors?.join('<br/>') || ''; // Concatena erros
-      const response = responseMessages + (responseErrors ? '<br/>' + responseErrors : '');
-      // Adiciona a entrada de histórico
-      this.history.push({ command: this.command, response: response });
-
-      // Limpa o comando atual
-      this.command = '';
-    },(error:any)=>{
-
-    })
   }
 }
--- src/app/interfaces/CommandDto.ts
+++ src/app/interfaces/CommandDto.ts
@@ -1,5 +1,4 @@
 // Definição da interface AnonymousDto (opcional, mas recomendado)
 export interface CommandDto {
-    userId: number
     content: string;
 }
